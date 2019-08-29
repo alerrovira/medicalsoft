329e32243afc7f39ee8f6ef1255b0564ba8ebb28
Aler Rovira <alerdavid@hotmail.com>
2019-08-29 08:45:21 -0500

    Iniciando proyecto medisoft

---
+++ src/Medicalsoft.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author estudiante
+ */
+public class Medicalsoft {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Medico primerMedico =new Medico("Aler","rovira");
+        
+        
+        primerMedico.getnombres=("";
+        primerMedico.apellidos="rovira";
+        System.out.println("primerMedico.nombre");
+    }
+    
+}
---
+++ src/Medico.java
@@ -0,0 +1,16 @@
+
+/**
+ *
+ * @author estudiante
+ */
+public class Medico extends Personas {
+    
+     Medico(String nom,String ape,String expec) {
+        
+        super(nom,ape);
+    }
+
+   
+    
+
+}
